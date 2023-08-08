3aba35111d92d5cf5c05ce4504febecab3962965
chl.cken <ckdgus988@naver.com>
2023-08-09 00:19:30 +0900

    FEATURE SEO용 메타 데이터 등록 방법 / title:

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -3,6 +3,13 @@ type Props = {
         slug: string;
     }
 }
+
+export function generateMetadata({ params }: Props) {
+    return {
+        title: `제품의 이름: ${params.slug}`,
+    }
+}
+    
 export default function Pants({params}: Props) {
     return <h1>{params.slug} 제품 설명 페이지!</h1>
 } 
