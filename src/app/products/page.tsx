9ffd5367e6f23da64aef7231445d557a6bd78f35
chl.cken <ckdgus988@naver.com>
2023-08-10 15:37:34 +0900

    FEATURE next.js 제공 Image 태그 사용 | 장점

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,17 +1,17 @@
 import { getProducts } from '@/service/products';
 import Link from 'next/dist/client/link';
 import MeowArticle from '@/components/MeowArticle';
-
+import clothesImage from '../../../public/images/clothes.jpg';
+import Image from 'next/image';
 // export const revalidate = 10;
 
-export default async function ProductsPage() {    
-    throw new Error();
-    
+export default async function ProductsPage() {        
     const products = await getProducts();
     
     return (
     <>
-        <h1>제품 소개 페이지!</h1>    
+        <h1>제품 소개 페이지!</h1>        
+        <Image src = {clothesImage} alt='Clothes'/>
         <ul>
             {products.map((product, index) => (
                 <li key={index}>
