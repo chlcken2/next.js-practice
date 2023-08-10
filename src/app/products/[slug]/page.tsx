dfaf942b16c6250eb3e52e30c6c6b1e98c04adaf
chl.cken <ckdgus988@naver.com>
2023-08-10 13:17:47 +0900

    FEATURE ISR 설정 방법, build - start로 실행, revalidate으로 타임 지정

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { getProduct, getProducts } from '@/service/products';
 
+export const revalidate = 10;
+
 type Props = {
     params: {
         slug: string;
@@ -12,15 +14,15 @@ export function generateMetadata({ params }: Props) {
     }
 }
     
-export default function PantsPage({params: {slug}}: Props) {
-    const product = getProduct(slug);
+export default async function ProductPage({params: {slug}}: Props) {
+    const product = await getProduct(slug);
 
-    return <h1>{product} 제품 설명 페이지!</h1>
+    return <h1>{product?.name} 제품 설명 페이지!</h1>
 } 
 
-export function generateStaticParams() {
-    const products = getProducts();
+export async function generateStaticParams() {
+    const products = await getProducts();
     return products.map(product => ({
-        slug: product,
-    }))
+        slug: product.id,
+    }));
 }
