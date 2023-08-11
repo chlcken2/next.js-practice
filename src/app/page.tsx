bd0493a622af1c5edaa9ae71bd8dbc50f0cec95c
chl.cken <ckdgus988@naver.com>
2023-08-11 15:28:19 +0900

    FEATURE ci/cd test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   console.log('안녕!');
   return (
   <>
-    <h1> 홈페이지! </h1>
+    <h1> 홈페이지! ver2.0</h1>
     <Counter/>
     <Image src = "https://images.unsplash.com/photo-1441986300917-64674bd600d8" 
     alt="shop"
